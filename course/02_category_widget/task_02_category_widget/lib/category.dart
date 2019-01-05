7cdac2994f5d9071856ac1f969a5c4bb38f2f5b6
Bogdan Orzea <bogdan.orzea@gmail.com>
2019-01-05 14:09:44 -0500

    Quiz 2 solution

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -6,6 +6,9 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+final _containerHeight = 100.0;
+final _borderRadius = BorderRadius.all(Radius.circular(_containerHeight / 2));
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
@@ -15,19 +18,62 @@ class Category extends StatelessWidget {
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  final String name;
+  final MaterialColor color;
+  final IconData iconLocation;
+
+  const Category(
+      {Key key,
+      @required this.name,
+      @required this.color,
+      @required this.iconLocation})
+      : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        super(key: key);
 
   /// Builds a custom widget that shows [Category] information.
   ///
   /// This information includes the icon, name, and color for the [Category].
   @override
-  // This `context` parameter describes the location of this widget in the
-  // widget tree. It can be used for obtaining Theme data from the nearest
-  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
+// This `context` parameter describes the location of this widget in the
+// widget tree. It can be used for obtaining Theme data from the nearest
+// Theme ancestor in the tree. Below, we obtain the display1 text theme.
+// See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    // Build the custom widget here, referring to the Specs.
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _containerHeight,
+        padding: EdgeInsets.all(8.0),
+        child: InkWell(
+          borderRadius: _borderRadius,
+          onTap: () {
+            print('I was tapped!');
+          },
+          highlightColor: color,
+          splashColor: color,
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(
+                  iconLocation,
+                  size: 60.0,
+                ),
+              ),
+              Center(
+                child: Text(
+                  name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
