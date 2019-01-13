9b3c91673a0bf499ee5c0f0284a3e7d0fb80c722
Bogdan Orzea <bogdan.orzea@gmail.com>
2019-01-12 21:45:07 -0500

    Quiz 6 solution

--- course/06_input/task_06_input/lib/converter_route.dart
+++ course/06_input/task_06_input/lib/converter_route.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'dart:math' as math;
 
 import 'unit.dart';
 
@@ -38,12 +39,65 @@ class ConverterRoute extends StatefulWidget {
 }
 
 class _ConverterRouteState extends State<ConverterRoute> {
-  // TODO: Set some variables, such as for keeping track of the user's input
-  // value and units
+  // Set some variables, such as for keeping track of the user's input value and units
+  bool _validInput = false;
 
-  // TODO: Determine whether you need to override anything, such as initState()
+  Unit _inputCurrentUnit;
+  Unit _outputCurrentUnit;
 
-  // TODO: Add other helper functions. We've given you one, _format()
+  final _inputTextController = TextEditingController();
+  final _outputTextController = TextEditingController();
+
+  List<DropdownMenuItem<Unit>> _unitsDropdownMenuItems;
+
+  RegExp regExp = RegExp(r"^\d*\.?\d*$");
+
+  // Determine whether you need to override anything, such as initState()
+  @override
+  void initState() {
+    _unitsDropdownMenuItems = _getDropdownMenuItems();
+    _inputCurrentUnit = _unitsDropdownMenuItems[0].value;
+    _outputCurrentUnit = _unitsDropdownMenuItems[1].value;
+    _inputTextController.addListener(() {
+      setState(() {
+        var inputText = _inputTextController.text;
+        _validInput = regExp.hasMatch(inputText);
+        if (_validInput) {
+          try {
+            var result = double.parse(inputText) /
+                _inputCurrentUnit.conversion *
+                _outputCurrentUnit.conversion;
+
+            _outputTextController.text = _format(result);
+          } on Exception catch (e) {
+            print('Error: $e');
+          }
+        }
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _inputTextController.dispose();
+    _outputTextController.dispose();
+
+    super.dispose();
+  }
+
+  // Add other helper functions. We've given you one, _format()
+  List<DropdownMenuItem<Unit>> _getDropdownMenuItems() {
+    return widget.units.map((Unit unit) {
+      return DropdownMenuItem<Unit>(
+        value: unit,
+        child: Text(
+          unit.name,
+        ),
+      );
+    }).toList();
+  }
 
   /// Clean up conversion; trim trailing zeros, e.g. 5.500 -> 5.5, 10.0 -> 10
   String _format(double conversion) {
@@ -63,39 +117,100 @@ class _ConverterRouteState extends State<ConverterRoute> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Create the 'input' group of widgets. This is a Column that
+    // Create the 'input' group of widgets. This is a Column that
     // includes the input value, and 'from' unit [Dropdown].
-
-    // TODO: Create a compare arrows icon.
-
-    // TODO: Create the 'output' group of widgets. This is a Column that
-    // includes the output value, and 'to' unit [Dropdown].
-
-    // TODO: Return the input, arrows, and output widgets, wrapped in a Column.
-
-    // TODO: Delete the below placeholder code.
-    final unitWidgets = widget.units.map((Unit unit) {
-      return Container(
-        color: widget.color,
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              unit.name,
-              style: Theme.of(context).textTheme.headline,
+    Widget inputGroup = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: _inputTextController,
+          style: Theme.of(context).textTheme.headline,
+          keyboardType: TextInputType.numberWithOptions(),
+          decoration: InputDecoration(
+            hasFloatingPlaceholder: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(4.0),
+              ),
             ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead,
-            ),
-          ],
+            labelText: 'Input',
+            errorText: _validInput ? null : 'Please input valid number',
+          ),
         ),
-      );
-    }).toList();
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: DropdownButton<Unit>(
+            value: _inputCurrentUnit,
+            items: _unitsDropdownMenuItems,
+            style: Theme.of(context).textTheme.headline,
+            isExpanded: true,
+            onChanged: (selectedUnit) {
+              setState(() {
+                _inputCurrentUnit = selectedUnit;
+              });
+            },
+          ),
+        ),
+      ],
+    );
 
-    return ListView(
-      children: unitWidgets,
+    // Create a compare arrows icon.
+    Widget compareArrowsGroup = Padding(
+      padding: _padding,
+      child: Transform.rotate(
+        angle: -math.pi / 2,
+        child: Icon(
+          Icons.compare_arrows,
+          size: 40.0,
+        ),
+      ),
+    );
+
+    // Create the 'output' group of widgets. This is a Column that
+    // includes the output value, and 'to' unit [Dropdown].
+    Widget resultGroup = Column(
+      children: <Widget>[
+        TextField(
+          controller: _outputTextController,
+          style: Theme.of(context).textTheme.headline,
+          enabled: false,
+          decoration: InputDecoration(
+            hasFloatingPlaceholder: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(4.0),
+              ),
+            ),
+            labelText: 'Result',
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: DropdownButton<Unit>(
+            value: _outputCurrentUnit,
+            items: _unitsDropdownMenuItems,
+            style: Theme.of(context).textTheme.headline,
+            isExpanded: true,
+            onChanged: (selectedUnit) {
+              setState(() {
+                _outputCurrentUnit = selectedUnit;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+
+    // Return the input, arrows, and output widgets, wrapped in a Column.
+    return Padding(
+      padding: _padding,
+      child: Column(
+        children: <Widget>[
+          inputGroup,
+          compareArrowsGroup,
+          resultGroup,
+        ],
+      ),
     );
   }
 }
